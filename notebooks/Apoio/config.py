dca22b609f02cbacd58abe698375a033b1cbac99
Antonio Manoel <antoniosouzaofs558@gmail.com>
2026-08-30 14:30:26 -0300

    teste

--- notebooks/Apoio/config.py
+++ notebooks/Apoio/config.py
@@ -19,7 +19,3 @@ DADOS_TRATADOS = PASTA_DADOS / "canada_tratados.parquet"
 # coloque abaixo o caminho para os arquivos de modelos de seu projeto
 PASTA_MODELOS = PASTA_PROJETO / "modelos"
 MODELO_FINAL = PASTA_MODELOS / "ridge.joblib"
-
-# coloque abaixo outros caminhos que você julgar necessário
-PASTA_RELATORIOS = PASTA_PROJETO / "relatorios"
-PASTA_IMAGENS = PASTA_RELATORIOS / "imagens"
